1e12ceea4d2f9107fec0e296a61cc2ef631ac99d
OlawaleJoseph <adedekojoseph@gmail.com>
2021-01-02 15:43:18 +0100

    setup tests

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,26 @@
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Enzyme, { shallow } from 'enzyme';
+import App from '../components/App';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
+const store = mockStore();
+
+describe('App', () => {
+  const page = shallow(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
+
+  test('should render home page', () => {
+    expect(page).toBeTruthy();
+    expect(page).not.toBeFalsy();
+    expect(page).toMatchSnapshot();
+  });
+});
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './scss/index.scss';
 import { Provider } from 'react-redux';
 import store from './store';
-import App from './App';
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
